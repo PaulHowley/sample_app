740bc76b1167b09c022b9690cb2e1cf909cdc90d
PaulHowley <paulhowley1976@googlemail.com>
2019-02-21 16:08:10 +0000

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello this is the rails tutorial sample app"
+  end
 end
