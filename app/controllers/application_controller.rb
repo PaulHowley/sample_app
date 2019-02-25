42667084721ef79cd38e66c57da1bd957d7cce56
PaulHowley <paulhowley1976@googlemail.com>
2019-02-25 09:55:58 +0000

    Finish static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-    render html: "hello this is the rails tutorial sample app"
-  end
 end
